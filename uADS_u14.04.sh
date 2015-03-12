a0719e55a2d597c6b2537f9dd3cbb8ae0517d58d
cbrookins <chris@brookins.info>
2015-03-12 11:18:33 -0400

    fixed reboot/lightdm/sed/variables

--- uADS_u14.04.sh
+++ uADS_u14.04.sh
@@ -18,12 +18,13 @@ read dc
 ## Install needed packages
 clear
 echo -e "${green}Installing needed packages${NC}"
-sudo apt-get -y install winbind libpam-winbind libnss-winbind krb5-user krb5-config libpam-krb5 samba >> /dev/null
-
+sudo apt-get -y install winbind libpam-winbind libnss-winbind samba >> /dev/null
+echo -e "${red}Enter $DOMAIN in the next screen${NC}"
+sudo apt-get -y install krb5-user krb5-config libpam-krb5
 
 ## Format and create additional variables
-DOMAIN=`echo DOMAIN | awk '{print toupper($0$)}'`
-domain=`echo DOMAIN | awk '{print tolower($0)}'`
+DOMAIN=`echo $DOMAIN | awk '{print toupper($0)}'`
+domain=`echo $DOMAIN | awk '{print tolower($0)}'`
 
 
 echo -e "${green}Configuring domain settings${NC}"
@@ -32,17 +33,17 @@ echo -e "${green}Configuring domain settings${NC}"
 ## Edit /etc/nsswitch.conf
 sudo cp /etc/nsswitch.conf /etc/nsswitch.conf.bk
 
-sudo sed '/^passwd:/ s/$/ winbind/' /etc/nsswitch.conf
-sudo sed '/^group:/ s/$/ winbind/' /etc/nsswitch.conf
+sudo sed -i '/^passwd:/ s/$/ winbind/' /etc/nsswitch.conf
+sudo sed -i '/^group:/ s/$/ winbind/' /etc/nsswitch.conf
 
 
 ## Edit /etc/krb5.conf
 sudo cp /etc/krb5.conf /etc/krb5.conf.bk
 sudo cp ./krb5.conf /etc/krb5.conf
 
-sudo sed -i 's/__DOMAIN__/$DOMAIN/g' /etc/krb5.conf
-sudo sed -i 's/__dc__/$dc/g' /etc/krb5.conf
-sudo sed -i 's/__domain/$domain/g' /etc/krb5.conf
+sudo sed -i 's/__DOMAIN__/'${DOMAIN}'/g' /etc/krb5.conf
+sudo sed -i 's/__dc__/'${dc}'/g' /etc/krb5.conf
+sudo sed -i 's/__domain__/'${domain}'/g' /etc/krb5.conf
 
 ## Edit /etc/samba/smb.conf
 sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.bk
@@ -58,7 +59,7 @@ echo "session required pam_mkhomedir.so skel=/etc/skel umask=0022" >> /etc/pam.d
 
 
 ## Edit /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf
-
+echo "greeter-show-manual-login=true" >> /usr/share/lightdm/lightdm-conf.d/50-ubuntu.conf
 
 ## Restart services
 echo -e "${green}Restarting services...${NC}"
@@ -72,6 +73,6 @@ sudo kinit $duser@$domain
 sudo net ads join -U $duser
 
 ## Reboot
-echo "Rebooting now..."
+echo "${green}Rebooting now...{$NC}"
 sleep 2
-sudo shutdown /r now
+sudo shutdown -r now
